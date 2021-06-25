73c8880f81f690ffe18ec231bc20e670857129a5
marco <marcotamcn@gmail.com>
2021-06-25 18:57:57 +0800

    Initial commit

---
+++ src/com/marco/terminal/layout.java
@@ -0,0 +1,8 @@
+package com.marco.terminal;
+
+public class layout {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the terminal version 2048");
+    }
+
+}
---
+++ src/com/marco/terminal/logic.java
@@ -0,0 +1,4 @@
+package com.marco.terminal;
+
+public class logic {
+}
