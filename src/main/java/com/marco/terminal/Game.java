65450361d9c7d34664b2af34e4683fae93ad43ef
marco <marcotamcn@gmail.com>
2021-06-26 14:10:17 +0800

    finish asciiTextGen

--- src/main/java/com/marco/terminal/Game.java
+++ src/main/java/com/marco/terminal/Game.java
@@ -2,23 +2,29 @@ package com.marco.terminal;
 
 import com.marco.terminal.ui.layout;
 
+import java.io.IOException;
+
 public class Game {
     // Init layout class here
     layout Layout = new layout();
     // Init logic class in here
     logic logic = new logic();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Game();
     }
 
-    public Game() {
+    public Game() throws IOException {
         welcome();
         logic.getInput();
     }
 
-    private void welcome() {
-        Layout.asciiTextGen("2048");
+    private void welcome() throws IOException {
+        try{
+            Layout.asciiTextGen("2048");
+        }catch (Exception e){
+            throw new IOException(e);
+        }
         // Welcome message
         System.out.println("Welcome to the terminal version 2048");
     }
--- src/main/java/com/marco/terminal/ui/layout.java
+++ src/main/java/com/marco/terminal/ui/layout.java
@@ -1,10 +1,46 @@
 package com.marco.terminal.ui;
 
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class layout {
-    public void asciiTextGen(String text){
-        int width , height;
+    private final Terminal terminal = TerminalBuilder.terminal();
 
+    public layout() throws IOException {
+    }
+
+    public void asciiTextGen(String text) {
+        int width, height;
+        width = terminal.getWidth();
+        height = terminal.getHeight();
+        if (width == 0) {
+            width = 90;
+        }
+        if (height == 0) {
+            height = 24;
+        }
+        BufferedImage stringImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = stringImage.getGraphics();
+        Graphics2D graphics2D = (Graphics2D) g;
+        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics2D.drawString(text, 1, 24);
+        for (int y = 0; y < height; y++) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int x = 0; x < width; x++) {
+                stringBuilder.append(stringImage.getRGB(x, y) == -16777216 ? " " : stringImage.getRGB(x, y) == -1 ? "#" : "*");
+            }
+
+            if (stringBuilder.toString().trim().isEmpty()) {
+                continue;
+            }
+
+            System.out.println(stringBuilder);
+        }
     }
 }
