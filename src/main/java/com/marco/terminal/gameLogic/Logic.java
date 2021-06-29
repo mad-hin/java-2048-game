77356aefb62feb17803d6b373f721d84b4486d36
marco <marcotamcn@gmail.com>
2021-06-29 16:21:35 +0800

    Finish the gameover function

--- src/main/java/com/marco/terminal/gameLogic/Logic.java
+++ src/main/java/com/marco/terminal/gameLogic/Logic.java
@@ -54,24 +54,106 @@ public class Logic {
     // Assign random number to the grid
     public void assignNumber(int[][] grid) {
         int assignPlace = (int) (Math.random() * (grid.length * grid.length));
+        boolean haveNotAssign = true;
+        while (haveNotAssign) {
+            int cnt = 0;
+            for (int i = 0; i < dimension; i++) {
+                for (int j = 0; j < dimension; j++) {
+                    if (cnt == assignPlace) {
+                        if (grid[i][j] == 0) {
+                            grid[i][j] = twoOrFour();
+                            haveNotAssign = false;
+                        }
+                    }
+                    cnt++;
+                }
+            }
+        }
+    }
 
+    // Choose generate 2 or 4 in the grid
+    private int twoOrFour() {
+        if ((int) (Math.random() * 10000000) % 2 == 0) {
+            return 4;
+        } else {
+            return 2;
+        }
     }
 
     // Start game
     public void startGame() {
-        while (!isGameOver()){
+        while (!isGameOver()) {
             assignNumber(grid);
             Layout.buildFrame(grid);
         }
 
-        if(isGameOver()){
+        if (isGameOver()) {
             gameover();
         }
     }
 
     // check game over
-    private boolean isGameOver(){
-        // TODO detect is the game over
+    private boolean isGameOver() {
+        return !canMoveLeft() && !canMoveRight() && !canMoveUp() && !canMoveDown();
+    }
+
+    // check the user can make a move to left
+    private boolean canMoveLeft() {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension - 1; j++) {
+                if (grid[i][j] == 0 && grid[i][j + 1] != 0) {
+                    return true;
+                }
+                if (grid[i][j] != 0 && grid[i][j + 1] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // check the user can make a move to right
+    private boolean canMoveRight() {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = dimension - 1; j > 0; j--) {
+                if (grid[i][j] == 0 && grid[i][j - 1] != 0) {
+                    return true;
+                }
+                if (grid[i][j] != 0 && grid[i][j - 1] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // check the user can make a move to up
+    private boolean canMoveUp() {
+        for (int i = 0; i < dimension - 1; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (grid[i][j] == 0 && grid[i + 1][j] != 0) {
+                    return true;
+                }
+                if (grid[i][j] != 0 && grid[i + 1][j] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // check the user can make a move to down
+    private boolean canMoveDown() {
+        for (int i = dimension - 1; i > 0; i--) {
+            for (int j = 0; j < dimension; j++) {
+                if (grid[i][j] == 0 && grid[i - 1][j] != 0) {
+                    return true;
+                }
+                if (grid[i][j] != 0 && grid[i - 1][j] == 0) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
